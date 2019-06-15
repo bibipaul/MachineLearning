9c045445c25294a675c22bafbf6629f7418ff111
bibipaul <47771936+bibipaul@users.noreply.github.com>
2019-06-15 07:45:51 +0100

    Update ExportDailyReturn.py

--- ExportDailyReturn.py
+++ ExportDailyReturn.py
@@ -2,7 +2,7 @@ import quandl
 
 class LoadReturn:
     def loadDailyReturn():
-        quandl.ApiConfig.api_key='8xsHs-1vCFmLsqD5Sfpr'
+        quandl.ApiConfig.api_key='your API key from quandl'
         stocks =['AAPL','AMZN','GOOGL','FB']
         data =quandl.get_table('WIKI/PRICES', ticker=stocks, qopts={'columns': ['date', 'ticker', 'adj_close']}
                       , date ={'gte': '2017-1-1', 'lte': '2018-12-31'}, paginate=True)
